edc9161afe9ef3436a3d5772313374e8adf37fcc
Drake Lundstrom <Drake.Lundstrom@neudesic.com>
2022-01-04 17:44:07 -0500

    changed url to point at correct API endpoint

--- scr/front-end/auction-website/src/Services/HttpClient.tsx
+++ scr/front-end/auction-website/src/Services/HttpClient.tsx
@@ -5,7 +5,7 @@ import { useEnvironmentVariables } from './useEnviromentVariables';
 export const get = async <T,>(uri: string, isSecure: boolean = true, isOverrideUrl: boolean = false) => {
   const { url, header } = getRequestArgs(uri, isSecure, isOverrideUrl);
   try {
-    const response = await axios.get('https://localhost:44389' + uri); //url, header);
+    const response = await axios.get(getBaseUrl + uri); //url, header);
     return { status: response.status, data: response.data };
   } catch (errorResponse) {
     return getErrors(errorResponse);
